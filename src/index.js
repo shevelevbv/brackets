71d38159f3ba5b0d62bce80d4b29367ec1db9959
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-06-21 17:15:59 -0400

    feat: solve Brackets

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ module.exports = function check(str, bracketsConfig) {
   for (let bracket of str) {
     for (let pattern of bracketsConfig) {
       if (pattern.includes(bracket)) {
-        if (bracket === pattern[1] && pattern[1] === openingBrackets[openingBrackets.length - 1]) {
+        if (bracket === pattern[1] && pattern[1] === openingBrackets[openingBrackets.length - 1]
+          || bracket === pattern[1] && pattern[0] === openingBrackets[openingBrackets.length - 1]) {
           openingBrackets.pop();
         } else if (bracket === pattern[0]) {
           openingBrackets.push(bracket);
-        } else if (bracket === pattern[1] && pattern[0] === openingBrackets[openingBrackets.length - 1]) {
-          openingBrackets.pop();
         } else {
           return false;
         }
