e03393b641d82511abd0f22e00c4c79af15cd9dc
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-06-21 17:13:47 -0400

    feat: solve Brackets

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = "";
-  let opening = [];
-  let closing = "";
-  let same = 0;
-  let same7 = 0;
-  let same8 = 0;
-
-  bracketsConfig.forEach(x => brackets += x[1]);
-
-  for (let i = 0; i < str.length; i++) {
-    if (!brackets.includes(str[i]) || (str[i] === "|" && same === 0) ||
-    (str[i] === "7" && same7 === 0) || (str[i] === "8" && same8 === 0)) {
-      opening.push(str[i]);
-      if (str[i] === "|") {
-        same++;
-      } else if (str[i] === "7") {
-        same7++;
-      } else if (str[i] === "8") {
-        same8++;
-      }
-    } else {
-      if (opening.length === 0) {
-        return false;
-      }
-      let filtered = bracketsConfig.filter(x => x[0] === opening[opening.length - 1]);
-      if (str[i] == filtered[0][1]) {
-        opening.pop();
-        if (str[i] === "|") {
-          same--;
-        } else if (str[i] === "7") {
-          same7--;
-        } else if (str[i] === "8") {
-          same8--;
+  let openingBrackets = [];
+  for (let bracket of str) {
+    for (let pattern of bracketsConfig) {
+      if (pattern.includes(bracket)) {
+        if (bracket === pattern[1] && pattern[1] === openingBrackets[openingBrackets.length - 1]) {
+          openingBrackets.pop();
+        } else if (bracket === pattern[0]) {
+          openingBrackets.push(bracket);
+        } else if (bracket === pattern[1] && pattern[0] === openingBrackets[openingBrackets.length - 1]) {
+          openingBrackets.pop();
+        } else {
+          return false;
         }
-      } else {
-        return false;
       }
     }
   }
-  return opening.length == 0 && same === 0 ? true : false;
+  return !openingBrackets.length;
 }
-
 
